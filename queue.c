5fae034848614bbf7795bd8e872eb53610fc9642
AndyWu <kk908676@gmail.com>
2025-03-02 22:20:55 +0800

    Refactor queue insertion functions to use list_add and list_add_tail
    Refactored q_insert_head and q_insert_tail to use list_add and
    list_add_tail instead of manually modifying list pointers. This improves
    code readability, reduces redundancy, and ensures correctness when
    inserting elements into a doubly linked list. Also added proper memory
    handling to prevent leaks when strdup fails.
    
    Change-Id: I0cf1f483dded8e1594125044d2334ed540344cae

--- queue.c
+++ queue.c
@@ -47,50 +47,41 @@ void q_free(struct list_head *head)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
-    element_t *item = (element_t *) malloc(sizeof(element_t));
-    if (!item) {
+    if (!head)
+        return false;
+
+    element_t *item = malloc(sizeof(element_t));
+    if (!item)
+        return false;
+
+    item->value = strdup(s);
+    if (!item->value) {
+        free(item);
         return false;
     }
-    item->value = strdup(s);
 
-    if (head->next == head) {
-        head->next = &item->list;
-        head->prev = &item->list;
-        item->list.next = head;
-        item->list.prev = head;
-    } else {
-        struct list_head *old = head->next;
-        head->next = &item->list;
-        old->prev = &item->list;
-        item->list.next = old;
-        item->list.prev = head;
-    }
-
+    list_add(&item->list, head);
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
-    element_t *item = (element_t *) malloc(sizeof(element_t));
+    if (!head)
+        return false;
+
+    element_t *item = malloc(sizeof(element_t));
     if (!item) {
         return false;
     }
-    item->value = strdup(s);
 
-    if (head->prev == head) {
-        head->next = &item->list;
-        head->prev = &item->list;
-        item->list.next = head;
-        item->list.prev = head;
-    } else {
-        struct list_head *old = head->prev;
-        head->prev = &item->list;
-        old->next = &item->list;
-        item->list.next = head;
-        item->list.prev = old;
+    item->value = strdup(s);
+    if (!item->value) {
+        free(item);
+        return false;
     }
 
+    list_add_tail(&item->list, head);
     return true;
 }
 
